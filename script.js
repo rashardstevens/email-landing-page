9422fb80af8740d46ca8c32539cfe9936a0ce91f
Rashard Stevens <rashardstevenswork@gmail.com>
2022-07-26 21:45:33 -0400

    -m "js file"

---
+++ script.js
@@ -0,0 +1,16 @@
+const scriptURL = "Your_Add_URL"; // email script link google execl
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Thank You For Subscribing!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
